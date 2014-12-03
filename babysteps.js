f7feb3783b4a37211377bdd71c47eb5c13c93222
cdg77 <cdegregorio@gmail.com>
2014-12-02 17:30:41 -0800

    baby steps passed

--- babysteps.js
+++ babysteps.js
@@ -1,15 +1,29 @@
 // console.log(process.argv);
 
-// console.log(+process.argv[2]);
+// console.log('sliced argv: ' + process.argv.slice(2));
 
-var inputArray = process.argv;
-var newArray = [];
+var inputArray = process.argv.slice(2);
+var total = [];
 
-console.log('inputArray: ' + inputArray);
 
-for (i=2; i<inputArray.length; i++) {
-	newArray = Number(inputArray[i]) + Number(inputArray[i]);
-	console.log('newArray inside: ' + newArray);
+// console.log('inputArray: ' + inputArray);
+
+// console.log(+inputArray[0] + (+inputArray[1]) + (+inputArray[2]));
+
+// console.log('total: ' + total);
+
+for (i=0; i<inputArray.length; i++) {
+	total = Number(inputArray[i]) + (+total);
+	// console.log('total inside: ' + total);
 }
-console.log('newArray outside: ' + newArray);
+console.log(total);
 
+// *******************************************************
+// Official Results :
+
+// var result = 0
+    
+//     for (var i = 2; i < process.argv.length; i++)
+//       result += Number(process.argv[i])
+    
+//     console.log(result)
